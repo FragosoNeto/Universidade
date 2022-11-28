b3e61ccdfb6096822125fe49200313aa7cf5470e
netofragoso <112201260+FragosoNeto@users.noreply.github.com>
2022-11-28 10:25:29 -0300

    Atualização

--- IHC-2/Log.py
+++ IHC-2/Log.py
@@ -3,7 +3,6 @@ from PyQt5 import  uic,QtWidgets
 import time
 import pandas as pd
 
-
 def chama_segunda_tela():
     nome_usuario = primeira_tela.lineEdit.text()
     senha = primeira_tela.lineEdit_2.text()
@@ -11,20 +10,32 @@ def chama_segunda_tela():
     bd = []
     with open(dados, 'r', encoding='utf-8') as dados :
         dt = csv.reader(dados, delimiter=';')
-        for line in dt :
+        for line in dt:
             bd.append(line)
     df = pd.DataFrame(bd, columns=['Nome', 'Login', 'Senha'])
-    c = df.loc[df['Login'] == nome_usuario]
+    d=(df["Login"]== nome_usuario)
+    for i in d:
+        if i==True:
+            c=df.loc[df['Login'] == nome_usuario]
+            print(c)
+            print(str(c.iat[0, 2]))
+            if str(c.iat[0, 2]) == senha:
+                primeira_tela.close()
+                segunda_tela.show()
+            else:
+                primeira_tela.label_4.setText("Dados de login incorretos!")
+        else:
+            primeira_tela.label_4.setText("Erro de Cadastro")
 
-    if str(c.iat[0, 2]) == senha:
-        primeira_tela.close()
-        segunda_tela.show()
-    else:
-        primeira_tela.label_4.setText("Dados de login incorretos!")
+def limpar():
+    primeira_tela.lineEdit.setText("")
+    primeira_tela.lineEdit_2.setText("")
+    primeira_tela.label_4.setText("")
 
 def logout():
     primeira_tela.lineEdit.setText("")
     primeira_tela.lineEdit_2.setText("")
+    primeira_tela.label_4.setText("")
     segunda_tela.close()
     primeira_tela.show()
 
@@ -35,12 +46,19 @@ def chama_primeira_tela():
     tela_cadastro.close()
     primeira_tela.show()
 
+def fechar_primeira_tela():
+    primeira_tela.lineEdit.setText("")
+    primeira_tela.lineEdit_2.setText("")
+    primeira_tela.label_4.setText("")
+    primeira_tela.close()
+
 def cadastrar():
     nome = tela_cadastro.lineEdit.text()
     login = tela_cadastro.lineEdit_2.text()
     senha = tela_cadastro.lineEdit_3.text()
     c_senha = tela_cadastro.lineEdit_4.text()
-
+    primeira_tela.lineEdit.setText("")
+    primeira_tela.lineEdit_2.setText("")
 
     if (senha == c_senha):
         try:
@@ -54,7 +72,6 @@ def cadastrar():
             tela_cadastro.lineEdit_3.setText("")
             tela_cadastro.lineEdit_4.setText("")
             tela_cadastro.close()
-
 
         except (ValueError, TypeError) as erro:
             print("Erro ao inserir os dados: ",erro)
@@ -71,9 +88,12 @@ primeira_tela.pushButton.clicked.connect(chama_segunda_tela)
 tela_cadastro.pushButton_2.clicked.connect(chama_primeira_tela)
 segunda_tela.pushButton.clicked.connect(logout)
 primeira_tela.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+tela_cadastro.lineEdit_3.setEchoMode(QtWidgets.QLineEdit.Password)
+tela_cadastro.lineEdit_4.setEchoMode(QtWidgets.QLineEdit.Password)
 primeira_tela.pushButton_2.clicked.connect(abre_tela_cadastro)
 tela_cadastro.pushButton.clicked.connect(cadastrar)
-
+primeira_tela.pushButton_3.clicked.connect(fechar_primeira_tela)
+primeira_tela.pushButton_4.clicked.connect(limpar)
 
 primeira_tela.show()
 app.exec()
--- IHC-2/text.py
+++ IHC-2/text.py
@@ -2,22 +2,36 @@ import csv
 import pandas as pd
 dados = 'dados.csv'
 bd=[]
-with open(dados, 'r', encoding='utf-8') as dados:
-    dt=csv.reader(dados,delimiter=';')
-    for line in dt:
-        bd.append(line)
-df=pd.DataFrame(bd, columns=['Nome','Login','Senha'])
-cl=[]
-dl=[]
-n="dbhskfhsdkjhf"
-senha="123456"
-
-
-if df.loc[df['Login'] == n] is not list:
-    print(1)
-# if str(c.iat[0,2])==senha:
+# with open(dados, 'r', encoding='utf-8') as dados:
+#     dt=csv.reader(dados,delimiter=';')
+#     for line in dt:
+#         bd.append(line)
+# df=pd.DataFrame(bd, columns=['Nome','Login','Senha'])
+# cl=[]
+# dl=[]
+# n="dbhskfhsdkjhf"
+# senha="123456"
+#
+#
+# if df.loc[df['Login'] == n] is not list:
 #     print(1)
+# # if str(c.iat[0,2])==senha:
+# #     print(1)
 
+nome_usuario = "ney"
+senha = "123456"
+dados = 'dados.csv'
+bd = []
+with open(dados, 'r', encoding='utf-8', newline='') as dados :
+    for linha in dados :
+        linha = linha.split(';')
+        bd.append(linha.split())
+        print(bd)
+    for nome_usuario in bd:
+        print(bd[1])
+        print(bd[2])
+        if nome_usuario == nome and senha == password :
+            print(1)
 
 
 
